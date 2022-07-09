a0e7dd04682eb2c1606417489a4ba37e847746f4
belalhamed <belalhamed@gmail.com>
2022-07-09 19:48:52 +0300

    fix youtube link

--- src/tyrolib/delphi/test/Perceptron.pas
+++ src/tyrolib/delphi/test/Perceptron.pas
@@ -6,7 +6,7 @@ unit Perceptron;
  *
  * @author    belalhamed.
  *
- * //check: https://www.youtube.com/watch?v=HyK_Q5rrcr4
+ * //check: https://www.youtube.com/watch?v=ntKn5TPHHAk&list=PLRqwX-V7Uu6aCibgK1PTWWu9by6XFdCfh&index=2
  *
  *}
 interface
